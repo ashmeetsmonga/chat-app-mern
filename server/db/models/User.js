a5a9dc94e122e6e8ccff4aebbee49c49c1983ab9
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-23 21:08:11 +0530

    password hashed before adding new user

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = mongoose.Schema(
 	{
@@ -18,5 +19,10 @@ const UserSchema = mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+UserSchema.pre("save", async function () {
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
 
 module.exports = mongoose.model("User", UserSchema);
