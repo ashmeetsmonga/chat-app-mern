534425e915743411ab9a8168d59c9eb9030da7f1
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-23 19:51:17 +0530

    register user controller added

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,9 +1,19 @@
+const { StatusCodes } = require("http-status-codes");
+const User = require("../db/models/User");
+
 const login = async (req, res) => {
 	res.status(200).json({ msg: "Login successful" });
 };
 
 const register = async (req, res) => {
-	res.status(200).json({ msg: "Register Successful" });
+	const { name, email, password } = req.body;
+
+	try {
+		const newUser = await User.create({ name, email, password });
+		res.status(StatusCodes.OK).json(newUser);
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 module.exports = { login, register };
