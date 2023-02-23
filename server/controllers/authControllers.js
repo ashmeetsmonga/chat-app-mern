d28ff6334ae6973f17cbcf4231cef1e037f06773
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-23 20:56:12 +0530

    bad request error added

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../db/models/User");
+const BadRequestError = require("../errors/BadRequestError");
 
 const login = async (req, res) => {
 	res.status(200).json({ msg: "Login successful" });
@@ -7,7 +8,8 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
 	const { name, email, password } = req.body;
-
+	if (!name || !email || !password)
+		throw new BadRequestError("Name, email and password are required");
 	try {
 		const newUser = await User.create({ name, email, password });
 		res.status(StatusCodes.OK).json(newUser);
