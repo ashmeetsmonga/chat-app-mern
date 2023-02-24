b1223820df86230138478c0ce11a27ce76eef191
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-24 23:46:58 +0530

    Message model added

---
+++ server/db/models/Message.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = mongoose.Schema({
+	chatId: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+		ref: "Chat",
+	},
+	senderId: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
