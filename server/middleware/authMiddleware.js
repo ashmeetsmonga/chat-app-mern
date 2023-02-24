ac79be29cf78fa312ca774896736316a35de4700
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-24 23:57:54 +0530

    auth middleware added

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const AuthenticationError = require("../errors/AuthenticationError");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+	const authHeaders = req.headers.Authorization;
+	if (!authHeaders || !authHeaders.startsWith("Bearer "))
+		throw new AuthenticationError("Token is missing");
+
+	const token = authHeaders.split(" ")[1];
+	try {
+		const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decodedToken;
+		next();
+	} catch (err) {
+		throw new AuthenticationError("Token verification failed");
+	}
+};
