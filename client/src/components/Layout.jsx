75f1fb7031fdd564e3c9fddbd618d2f3462a728b
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-27 23:04:29 +0530

    added layout css

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Layout = () => {
-	return <div>Layout</div>;
+	return (
+		<div className='w-full h-full flex'>
+			<div className='w-1/3'>Chat List</div>
+			<div className='w-2/3'>Single Chat</div>
+		</div>
+	);
 };
 
 export default Layout;
