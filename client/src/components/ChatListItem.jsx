606c0a37b46faa811ae69d694909e2ad4b454362
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-02 18:09:04 +0530

    ui styling

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -3,13 +3,13 @@ import { BsFillPersonFill } from "react-icons/bs";
 
 const ChatListItem = () => {
 	return (
-		<div className='flex justify-around p-4 items-center border-b border-b-gray-100'>
+		<div className='flex py-2 px-8 gap-6 items-center border-b border-b-gray-100'>
 			<div>
-				<BsFillPersonFill />
+				<BsFillPersonFill className='w-10 h-10 text-white' />
 			</div>
 			<div>
-				<div className='text-white text-2xl tracking-wider'>Avneet</div>
-				<div>Last Message</div>
+				<div className='text-white text-2xl tracking-wider font-bold'>Avneet</div>
+				<div className='text-lg text-red-200 tracking-wider'>Last Message</div>
 			</div>
 		</div>
 	);
