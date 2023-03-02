02d028e57ab0d74d493a1e62eccafee47de263b0
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-02 18:29:17 +0530

    read name from data in ui

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { BsFillPersonFill } from "react-icons/bs";
 
-const ChatListItem = () => {
+const ChatListItem = ({ chatData }) => {
+	console.log(chatData);
 	return (
 		<div className='flex py-2 px-8 gap-6 items-center border-b border-b-gray-100'>
 			<div>
 				<BsFillPersonFill className='w-10 h-10 text-white' />
 			</div>
 			<div>
-				<div className='text-white text-2xl tracking-wider font-bold'>Avneet</div>
+				<div className='text-white capitalize text-2xl tracking-wider font-bold'>
+					{chatData.name}
+				</div>
 				<div className='text-lg text-red-200 tracking-wider'>Last Message</div>
 			</div>
 		</div>
