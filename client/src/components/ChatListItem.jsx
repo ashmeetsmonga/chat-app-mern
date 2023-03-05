f18af329ed94bc83f90b5995d78800e5bcbf7c1e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-05 17:18:56 +0530

    ui changes in chatlist

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { BsFillPersonFill } from "react-icons/bs";
 
 const ChatListItem = ({ chatData, randomColor }) => {
+	console.log(chatData);
 	return (
-		<div className='flex py-2 px-8 gap-6 items-center border-b border-b-gray-100'>
-			<div className={`p-4 ${randomColor} rounded-full `}>
-				<BsFillPersonFill className='w-10 h-10 text-white' />
+		<div className='flex py-4 px-6 gap-6 items-center'>
+			<div className={`p-3 ${randomColor} rounded-full `}>
+				<BsFillPersonFill className='w-6 h-6 text-white' />
 			</div>
 			<div>
-				<div className='text-white capitalize text-2xl tracking-wider font-bold'>
-					{chatData.name}
-				</div>
-				<div className='text-lg text-red-200 tracking-wider'>Last Message</div>
+				<div className='text-white capitalize text-xl'>{chatData.name}</div>
+				<div className='text-sm tracking-wide text-gray-400'>Last Message</div>
 			</div>
 		</div>
 	);
