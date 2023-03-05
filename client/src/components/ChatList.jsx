f2a43f53c59ed5954b51ccc42f8905416a4129f8
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-05 16:56:16 +0530

    ui changes in chatlist

--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { RiAddFill } from "react-icons/ri";
+import { RiAddFill, RiSearch2Line } from "react-icons/ri";
 import { useMutation, useQuery } from "react-query";
 import { useCreateNewChat } from "../api/query-hooks/useCreateNewChat";
 import { useGetAllChats } from "../api/query-hooks/useGetAllChats";
@@ -27,27 +27,30 @@ const ChatList = () => {
 
 	return (
 		<div className='w-full h-full flex flex-col gap-4 bg-[#161B1D]'>
-			<div className='flex justify-between px-8 py-8 items-center border border-gray-700 border-l-0 border-t-0'>
+			<div className='flex justify-between p-6 items-center border border-gray-700 border-l-0 border-t-0'>
 				<div className='flex gap-6 items-center'>
-					<div className='rounded-full p-4 bg-purple-500'>
+					<div className='rounded-full p-3 bg-purple-500'>
 						<BsFillPersonFill className='w-6 h-6 text-white' />
 					</div>
 					<div className=''>
-						<div className='text-white text-xl font-[500]'>Ashmeet Singh</div>
-						<div className='text-gray-400 text-lg'>My Account</div>
+						<div className='text-white text-lg font-[500]'>Ashmeet Singh</div>
+						<div className='text-gray-400 text-md'>My Account</div>
 					</div>
 				</div>
-				<AiOutlineLogout className='w-10 h-10 text-gray-500 cursor-pointer hover:text-white transition-colors' />
+				<AiOutlineLogout className='w-9 h-9 text-gray-500 cursor-pointer hover:text-white transition-colors' />
 			</div>
-			<div className='w-full p-4 flex flex-col gap-4 items-center'>
+			<div className='w-full flex flex-col gap-4 p-6 items-center'>
 				<form className='w-full flex justify-center relative' onSubmit={addNewChat}>
-					<input
-						className='w-full text-white p-5 rounded-xl bg-[#1D2122] border placeholder:text-[#595D5D] border-gray-700 outline-none'
-						type='text'
-						placeholder='Create New Chat (Email)...'
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
+					<label className='relative w-full focus-within:text-white'>
+						<RiSearch2Line className='absolute w-8 h-8 top-1/2 -translate-y-1/2 left-3 pointer-events-none text-[#595D5D] ' />
+						<input
+							className='w-full text-white p-4 pl-14 rounded-xl bg-[#1D2122] border placeholder:text-[#595D5D] border-gray-700 outline-none'
+							type='text'
+							placeholder='Create New Chat (Email)...'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</label>
 					{/* <button className='-ml-12' type='submit'>
 						<RiAddFill className='w-10 h-10 text-red-700' />
 					</button> */}
